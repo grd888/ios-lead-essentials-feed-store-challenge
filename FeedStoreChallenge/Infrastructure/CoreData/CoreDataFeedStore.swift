d4b786feed0759ca86ff8ae0fbc5ac92d8f5e038
Greg Delgado <grd888@gmail.com>
2021-09-03 06:47:04 +0800

    CoreDataFeedStore.deleteCachedFeed() delivers error on deletion error

--- FeedStoreChallenge/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ FeedStoreChallenge/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -62,6 +62,17 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		let context = self.context
+		context.perform {
+			do {
+				try ManagedCache.find(in: context)
+					.map(context.delete)
+					.map(context.save)
+				completion(nil)
+			} catch {
+				context.rollback()
+				completion(error)
+			}
+		}
 	}
 }
